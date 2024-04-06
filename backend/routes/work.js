5e06a4e19837003c676fbd2b1c5937ae32a6824f
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-06 18:30:40 +0200

    Portfolio_backend-work-routes update
    Added auth middleware on POST, PATCH, and DELETE routes.

--- backend/routes/work.js
+++ backend/routes/work.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const worksCtrl = require('../controllers/work');
 
 const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth');
 
 router
     .route('/')
     .get(worksCtrl.getAllWorks)
     .post(
+        auth,
         multer.uploadWorkImages,
         multer.resizeWorksImages,
         worksCtrl.createWork
@@ -18,10 +20,14 @@ router
     .route('/:id')
     .get(worksCtrl.getOneWork)
     .patch(
+        auth,
         multer.uploadWorkImages,
         multer.resizeWorksImages,
         worksCtrl.modifyWork
     )
-    .delete(worksCtrl.deleteWork);
+    .delete(
+        auth,
+        worksCtrl.deleteWork
+    );
 
 module.exports = router;
