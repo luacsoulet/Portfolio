437c480e69d242f9c1d551ee1ba4b7151c05bf31
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-06 14:12:22 +0200

    Portfolio_backend-multer-config-middleware create
    Implementation of multer on creation and modification routes to save and compress images in the right format and then add access links to the sliderImages array of objects.

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+const sharp = require('sharp');
+
+const multerStorage = multer.memoryStorage();
+
+const upload = multer({
+    storage: multerStorage
+});
+
+exports.uploadWorkImages = upload.array('sliderImages', 5);
+
+exports.resizeWorksImages = async (req, res, next) => {
+    if (!req.files) return next();
+
+    req.body.sliderImages = [];
+
+    await Promise.all(
+        req.files.map(async (file, i) => {
+            const filename = `work-${Date.now()}-${i + 1}.png`;
+
+            await sharp(file.buffer)
+                .toFormat('png')
+                .png({ quality: 90 })
+                .toFile(`./images/${filename}`);
+
+            req.body.sliderImages.push(`${req.protocol}://${req.get('host')}/images/${filename}`);
+        })
+    );
+
+    next();
+};
