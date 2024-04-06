019c6fe1d951a86f61e4af61a0338f0a09a5ca2a
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-06 14:13:56 +0200

    Portfolio_backend-works-routes update
    Added the middleware multer and its image download and compression middleware to the POST and PATCH routes.

--- backend/routes/work.js
+++ backend/routes/work.js
@@ -3,15 +3,25 @@ const router = express.Router();
 
 const worksCtrl = require('../controllers/work');
 
+const multer = require('../middleware/multer-config');
+
 router
     .route('/')
     .get(worksCtrl.getAllWorks)
-    .post(worksCtrl.createWork);
+    .post(
+        multer.uploadWorkImages,
+        multer.resizeWorksImages,
+        worksCtrl.createWork
+    );
 
 router
     .route('/:id')
     .get(worksCtrl.getOneWork)
-    .patch(worksCtrl.modifyWork)
+    .patch(
+        multer.uploadWorkImages,
+        multer.resizeWorksImages,
+        worksCtrl.modifyWork
+    )
     .delete(worksCtrl.deleteWork);
 
 module.exports = router;
