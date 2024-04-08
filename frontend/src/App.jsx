41f540193dadf0eb2d16f2383a2d1472cba4bb10
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-08 21:20:07 +0200

    Portfolio_frontend-App.jsx update
    Added component header in the application as well as importing BrowserRoutes to better separate pages and components in the application router.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Header from './components/Header/Header'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
