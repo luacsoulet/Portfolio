a4fdcf654426d7efe6b0953bb10f448cfeaded12
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-08 16:36:28 +0200

    Portfolio_frontend-app.jsx update
    Import the Home.Jsx page into the router file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import Home from './pages/Home/Home'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Home />
   </React.StrictMode>,
 )
