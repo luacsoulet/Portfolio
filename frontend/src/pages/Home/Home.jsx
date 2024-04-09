0d2d8ee9ce4713792c717717ec72ff5c6b23a080
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-09 04:18:54 +0200

    Portfolio_frontend-Home-pages.jsx update
    Update of the className.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,13 +3,13 @@ import './Home.sass';
 function Home(){
 
     return (
-        <div>
-            <div className="home-intro">
-                <p className="home-intro-first-sentence">I{"'"}m a passionate web developer,</p>
-                <p className="home-intro-second-sentence">I love learning and discovering new things about web development. You{"'"}ll find all the projects I{"'"}ve worked on here.</p>
+        <div className='home'>
+            <div className="home__intro">
+                <p className="home__intro-first-sentence">I{"'"}m a passionate web developer,</p>
+                <p className="home__intro-second-sentence">I love learning and discovering new things about web development. You{"'"}ll find all the projects I{"'"}ve worked on here.</p>
             </div>
-            <div className="home-gallery">
-                <div className="home-gallery-container"></div>
+            <div className="home__gallery">
+                <div className="home__gallery-container"></div>
             </div>
         </div>
     )
