60b120e04b570a9ced297e7f47f8ab7d8ea96381
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-11 00:27:25 +0200

    Portfolio_frontend-Card-component.jsx update
    Add Props in the component and display them and set Card propTypes.

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,12 +1,30 @@
+import PropTypes from 'prop-types';
 import './Card.sass'
 
-function Card(){
+function Card({title, image, tags}) {
 
-    return(
-        <div>
-            
+    console.log(tags)
+    return (
+        <div className='work'>
+            <div className='work__image'><img src={image} alt={title} /></div>
+            <div className='work__content'>
+                <p className='work__content-title'>{title}</p>
+                <div className='work__content-tags'>
+                    {tags.map((tag , i) => (
+                        <div key={`tag-${i}`} className='tag__container'>
+                            <p className='tag__container-text'>{tag}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
+}
+
+Card.propTypes = {
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    tags: PropTypes.array.isRequired,
 }
 
 export default Card;
