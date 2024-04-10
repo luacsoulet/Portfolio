c69dc2590029874a94216f474e19599a54640ace
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-11 00:30:34 +0200

    Portfolio_frontend-Home-page.jsx update
    Added the useState for the data to be generated by the API request and then render the data by passing them in props to the Card component.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react';
+import Card from '../../components/Card/Card'
 import './Home.sass';
 
 function Home(){
+    const [workData, setWorkData] = useState([]);
+    // let workData = [];
 
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('http://localhost:4000/api/v1/works');
+            const json = await response.json();
+            setWorkData(json.works);
+        }
+        
+        fetchData();
+    }, []); 
+    
     return (
         <div className='home'>
             <div className="home__intro">
@@ -9,7 +23,22 @@ function Home(){
                 <p className="home__intro-second-sentence">I love learning and discovering new things about web development. You{"'"}ll find all the projects I{"'"}ve worked on here.</p>
             </div>
             <div className="home__gallery">
-                <div className="home__gallery-container"></div>
+                <div className='home__gallery-title'>Work</div>
+                <div className='home__gallery-container'>
+                    {workData && workData.length > 0 ? (
+
+                        workData.map((work, i) => (
+                            <Card 
+                                key={`work-${i}`}
+                                title={work.title}
+                                image={work.imageCover}
+                                tags={work.tags}
+                            />
+                        ))
+                    ) : (
+                        <p>Chargement...</p>
+                    )}
+                </div>
             </div>
         </div>
     )
