a23866a5f66dee015d4942c17ae1fdc1ed5da448
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-04-13 18:30:21 +0200

    Portfolio_frontend-App.jsx update
    Added the Work page to display additional information about the selected project. And the Error page for non-existing addresses or pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import WorkContainer from './components/WorkContainer/WorkContainer'
+import Error from './pages/Error/Error'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import './styles/index.sass'
@@ -12,6 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path="/work/:id" element={<WorkContainer />} />
+        <Route path='*' element={<Error />}/>
       </Routes>
       <Footer />
     </Router>
